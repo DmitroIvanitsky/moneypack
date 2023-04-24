9b56b3e21123d25b1749aab7b55c3702665706c6
Dmitry Ivanitsky <onemore43@gmail.com>
2023-04-24 14:22:18 +0200

    First stage of refactoring, for an article

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,52 @@
-import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:money_pack/Objects/ListOfExpenses.dart';
-import 'package:money_pack/Objects/ListOfIncomes.dart';
-import 'package:money_pack/Utility/Storage.dart';
-import 'package:money_pack/pages/AddExpense.dart';
-import 'package:money_pack/pages/AddIncome.dart';
-import 'package:money_pack/pages/Balance.dart';
-import 'package:money_pack/pages/Expenses.dart';
-import 'package:money_pack/pages/Incomes.dart';
-import 'package:money_pack/pages/notes.dart';
-import 'package:money_pack/setting/AppDecoration.dart';
-import 'package:money_pack/setting/MainLocalText.dart';
-import 'package:money_pack/setting/AppColors.dart';
-import 'package:money_pack/setting/SecondaryText.dart';
-import 'package:money_pack/widgets/DateWidget.dart';
-import 'package:money_pack/widgets/AppDropdownButton.dart';
-import 'package:money_pack/widgets/rowWithButton.dart';
-import 'Utility/appLocalizations.dart';
-import 'package:gx_file_picker/gx_file_picker.dart';
+import 'package:money_pack/services/backup_service.dart';
+import 'package:money_pack/services/navigator_service.dart';
 
-void main() => runApp(MaterialApp(
-  theme: ThemeData(fontFamily: 'main',),
+import 'pages/main_page.dart';
+import 'repository/persistent_storage.dart';
+import 'services/income_service.dart';
+import 'services/expense_service.dart';
+import 'Utility/app_localizations.dart';
+import 'decorations/theme_data.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferencesStorage storage = await SharedPreferencesStorage.initialize();
+
+  IncomeService incomeService = await IncomeService.initialize(
+      storage: storage,
+      firstSampleCategory: 'Salary'
+  );
+
+  ExpenseService expenseService = await ExpenseService.initialize(
+      storage: storage,
+      firstSampleCategory: 'Food'
+  );
+
+  NavigatorService navigatorService = await NavigatorService.initialize(
+      incomeService: incomeService,
+      expenseService: expenseService
+  );
+
+  BackupService backupService = await BackupService.initialize(
+    storage: storage,
+    expenseServ: expenseService,
+    incomeServ: incomeService
+  );
+
+  SystemChrome.setPreferredOrientations([ //Lock orientation
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(MaterialApp(
+  theme: AppTheme.lightTheme, //light theme
+  darkTheme: AppTheme.darkTheme,
   themeMode: ThemeMode.system,
   debugShowCheckedModeBanner: false,
   localizationsDelegates: [
-    // A class which loads the translations from JSON files
     AppLocalizationsDelegate(),
     GlobalMaterialLocalizations.delegate,
     GlobalWidgetsLocalizations.delegate,
@@ -40,273 +58,11 @@ void main() => runApp(MaterialApp(
     const Locale.fromSubtags(languageCode: 'ru'),
     const Locale.fromSubtags(languageCode: 'uk'),
   ],
-  home: MoneyPack(),
+  home: MainPage(
+    incomeService: incomeService,
+    expenseService: expenseService,
+    navigatorService: navigatorService,
+    backupService: backupService,
+  ),
 ));
-
-class MoneyPack extends StatefulWidget {
-  @override
-  _MoneyPackState createState() => _MoneyPackState();
-}
-
-class _MoneyPackState extends State<MoneyPack> {
-  DateTime date = new DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
-  String selectedMode = 'День';
-  double income = 0;
-  double expense = 0;
-  double balance = 0;
-  double totalBalance = 0;
-
-  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-
-  @override
-  void initState() {
-    SystemChrome.setPreferredOrientations([ //Lock orientation
-      DeviceOrientation.portraitUp,
-    ]);
-    loadList();
-    super.initState();
-  }
-
-  void loadList() async {
-    String expN = await Storage.getString('ExpenseNote');
-    String incN = await Storage.getString('IncomeNote');
-    setState(() {
-      if (expN != null) ListOfExpenses.fromJson(jsonDecode(expN));
-      if (incN != null) ListOfIncomes.fromJson(jsonDecode(incN));
-    });
-
-    updateMainPage();
-  }
-
-  void updateDate(DateTime dateTime) {
-      date = dateTime;
-      updateMainPage();
-  }
-
-  void updateSelectedMode(String selMode){
-    selectedMode = selMode;
-    updateMainPage();
-  }
-
-  void updateMainPage({selMode}) async {
-    setState(() {
-      income = filterSumByPeriod(ListOfIncomes.filtered(selMode: selectedMode, currentDate: date));
-      expense = filterSumByPeriod(ListOfExpenses.filtered(selMode: selectedMode, currentDate: date));
-      balance = income - expense;
-      totalBalance = remainFunc();
-    });
-  }
-
-  double remainFunc(){
-    return ListOfIncomes.sum() - ListOfExpenses.sum();
-  }
-
-  double filterSumByPeriod(List list){
-    double sum = 0;
-    for (int i = 0; i < list.length; i++) {
-        sum += list[i].sum;
-    }
-    return sum;
-  }
-
-  buildDrawer(){
-    return Container(
-      width: 350,
-      child: Drawer(
-        child: Container(
-          color: AppColors.backGroundColor(),
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 25),
-                decoration: AppDecoration.boxDecoration(context),
-                child: FlatButton(
-                  child: MainLocalText(text: 'сохранить резервную копию'),
-                  onPressed: () => Storage.saveBackup(),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 25),
-                decoration: AppDecoration.boxDecoration(context),
-                child: FlatButton(
-                  child: MainLocalText(text: 'загрузить резервную копию'),
-                  onPressed: () async {
-                    File file =  await FilePicker.getFile(
-                        type: FileType.custom,
-                        allowedExtensions: ['json']
-                    );
-                    if (file != null) {
-                      await Storage.loadBackup(file);
-                      await loadList();
-                      updateMainPage();
-                      Navigator.of(context).pop();
-                    }
-                  }
-                ),
-              ),
-              // Container(
-              //   margin: EdgeInsets.only(top: 25),
-              //   decoration: AppDecoration.boxDecoration(context),
-              //   child: FlatButton(
-              //     child: MainLocalText(text: 'Заметки'),
-              //     onPressed: () =>
-              //       Navigator.push(
-              //         context,
-              //         MaterialPageRoute(
-              //           builder: (context) => Notes(),
-              //         ),
-              //       ),
-              //   ),
-              // ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Storage.langCode = AppLocalizations.of(context).locale.toString();
-    Storage.brightness = MediaQuery.of(context).platformBrightness;
-
-    return SafeArea(
-        child: Scaffold(
-            key: scaffoldKey,
-            endDrawerEnableOpenDragGesture: true,
-            resizeToAvoidBottomInset: false,
-            backgroundColor: AppColors.backGroundColor(),
-            drawer: buildDrawer(),
-            appBar: AppBar(
-              shadowColor: AppColors.backGroundColor().withOpacity(.01),
-              iconTheme: IconThemeData(color: AppColors.textColor()),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  MainLocalText(text: 'Учёт'),
-                  AppDropdownButton(page: 'main', selectedMode: selectedMode, updateSelMode: updateSelectedMode,)
-                ],
-              ),
-              centerTitle: true,
-              backgroundColor: AppColors.backGroundColor(),
-            ),
-            body: Container(
-              height: MediaQuery.of(context).size.height,
-              child:  Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: Container(
-                      height: 50,
-                      width: 300,
-                      decoration: AppDecoration.boxDecoration(context),
-                      child: DateWidget(selMode: selectedMode, date: date, update: updateDate, color: AppColors.textColor()),
-                    ),
-                  ),
-                  RowWithButton(
-                    leftText: 'Доход',
-                    rightText: income.toStringAsFixed(2),
-                    onTap: () =>
-                        Navigator.push(context,
-                          MaterialPageRoute(
-                              builder: (context) => Incomes(updateMainPage: updateMainPage)
-                          ),
-                        ),
-                  ),
-                  RowWithButton(
-                    leftText: 'Расход',
-                    rightText: expense.toStringAsFixed(2),
-                    onTap: () =>
-                        Navigator.push(context,
-                          MaterialPageRoute(
-                              builder: (context) => Expenses(updateMainPage: updateMainPage)
-                          ),
-                        ),
-                  ),
-                  RowWithButton(
-                    leftText: 'Баланс',
-                    rightText: balance.toStringAsFixed(2),
-                    onTap: () =>
-                        Navigator.push(context,
-                          MaterialPageRoute(
-                              builder: (context) => Balance(updateMainPage: updateMainPage)
-                          ),
-                        ),
-                  ),
-                  Container(
-                      decoration: AppDecoration.boxDecoration(context),
-                      height: 50,
-                      margin: EdgeInsets.only(left: 20, right: 20, top: 0),
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 15, right: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                MainLocalText(text: 'Общий остаток'),
-                                SecondaryText(text: totalBalance.toStringAsFixed(2),),
-                              ],
-                            ),
-                          ],
-                        ),
-                      )
-                  ),
-                  // low buttons to add notes
-                  Container(
-                    alignment: Alignment.center,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 15),
-                          child: Container(
-                            decoration: AppDecoration.incomeButtonDecoration(context),
-                            height: 70,
-                            width: 215,
-                            child: FlatButton(
-                              child: MainLocalText(
-                                text: 'Добавить доход',
-                                color: Storage.brightness == Brightness.light ? AppColors.textColor() : AppColors.incomeButton
-                              ),
-                              onPressed: () =>
-                                  Navigator.push(context,
-                                    MaterialPageRoute(
-                                        builder: (context) => AddIncome(callback: updateMainPage)
-                                    ),
-                                  ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 15),
-                          child: Container(
-                            decoration: AppDecoration.expenseButtonDecoration(context),
-                            height: 70,
-                            width: 215,
-                            child: FlatButton(
-                              child: MainLocalText(
-                                text: 'Добавить расход',
-                                color: Storage.brightness == Brightness.light ? AppColors.textColor() : AppColors.expenseButton
-                              ),
-                              onPressed: () =>
-                                  Navigator.push(context,
-                                    MaterialPageRoute(
-                                        builder: (context) => AddExpenses(callBack: updateMainPage)),
-                                  ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            )
-        )
-    );
-  }
-
 }
--- lib/pages/notes.dart
+++ lib/pages/notes.dart
@@ -1,175 +1,165 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import '../setting/AppDecoration.dart';
-import '../setting/SecondaryText.dart';
-import '../Utility/appLocalizations.dart';
-import '../widgets/customSnackBar.dart';
-import '../setting/MainLocalText.dart';
-import '../Utility/Storage.dart';
-import '../setting/AppColors.dart';
-
-class Notes extends StatefulWidget {
-
-  @override
-  _NotesState createState() => _NotesState();
-}
-
-class _NotesState extends State<Notes> {
-  List<String> list = [];
-  String tempField = '';
-  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
-  FocusNode addCatFocusNode;
-
-  @override
-  void initState() {
-    initList();
-    addCatFocusNode = FocusNode();
-    super.initState();
-  }
-
-  void dispose() {
-    addCatFocusNode.dispose();
-    super.dispose();
-  }
-
-  initList() async {
-    if (await Storage.getList('Notes') != null)
-      list = await Storage.getList('Notes');
-    updateList();
-  }
-
-  void updateList() {
-    setState(() {
-      list.sort();
-    });
-  }
-
-  void undoDelete(String category, int index) async {
-    if (index < list.length)
-      list.insert(index, category);
-    else
-      list.add(category);
-
-    await Storage.saveList(list, "Notes");
-    updateList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: AppColors.backGroundColor(),
-        appBar: AppBar(
-          shadowColor: AppColors.backGroundColor().withOpacity(.001),
-          backgroundColor: AppColors.backGroundColor(),
-          iconTheme: IconThemeData(color: AppColors.textColor()),
-          title: MainLocalText(text: "Заметки"),
-        ),
-        body: Column(
-          children: [
-            SizedBox(height: 10),
-            Expanded(
-              child: list.isEmpty
-                ? Center(child: MainLocalText(text: 'заметок нет'))
-                : ListView.builder(
-                    itemCount: list.length,
-                    itemBuilder: (context, index) {
-                      String category = list[index];
-                      return Dismissible(
-                        key: ValueKey(category),
-                        child: Padding(
-                        padding: EdgeInsets.only(left: 5, right: 5),
-                          child: Column(
-                            children: [
-                              SizedBox(height: 25),
-                              Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [Expanded(child: SecondaryText(text: category))]
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        direction: DismissDirection.endToStart,
-                        onDismissed: (direction) async {
-                          CustomSnackBar.show(
-                              key: scaffoldKey,
-                              context: context,
-                              text: AppLocalizations.of(context).translate('Удалена заметка: ') + category,
-                              textColor: Colors.white,
-                              callBack: () {
-                                undoDelete(category, index);
-                              });
-                          list.remove(category);
-                          await Storage.saveList(list, 'Notes');
-                          updateList();
-                        },
-                        background: Container(),
-                        secondaryBackground: Container(
-                          alignment: Alignment.centerRight,
-                          color: Colors.redAccent,
-                          child: Padding(
-                            padding: EdgeInsets.only(right: 15),
-                            child: Icon(Icons.clear, color: Colors.black54),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: AppDecoration.boxDecoration(context),
-                      child: TextFormField(
-                        style: TextStyle(color: AppColors.textColor()),
-                        controller: TextEditingController(),
-                        decoration: InputDecoration(
-                          hintText: AppLocalizations.of(context).translate('Добавьте заметку'),
-                          hintStyle: TextStyle(color: AppColors.hintColor),
-                          contentPadding: EdgeInsets.all(20.0),
-                          border: addCatFocusNode.hasFocus
-                            ? OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                              borderSide: BorderSide(color: Colors.blue)
-                              )
-                            : InputBorder.none,
-                        ),
-                        onChanged: (v) => tempField = v,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 25),
-                    child: Container(
-                      height: 60,
-                      width: 60,
-                      decoration: AppDecoration.boxDecoration(context),
-                      child: IconButton(
-                        icon: Icon(Icons.add, color: AppColors.textColor()),
-                        onPressed: () async {
-                          if (tempField == '') return;
-                          list.add(tempField);
-                          tempField = '';
-                          TextEditingController().clear();
-                          await Storage.saveList(list, "Notes");
-                          updateList();
-                        },
-                      ),
-                    ),
-                  )
-                ]
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:money_pack/services/notes_service.dart';
+// import '../decorations/AppDecoration.dart';
+// import '../Utility/appLocalizations.dart';
+// import '../widgets/large_text.dart';
+// import '../widgets/medium_text.dart';
+// import '../widgets/customSnackBar.dart';
+// import '../constants/AppColors.dart';
+//
+// class NotesPage extends StatefulWidget {
+//
+//   @override
+//   _NotesPageState createState() => _NotesPageState();
+// }
+//
+// class _NotesPageState extends State<NotesPage> {
+//   List<String> list = [];
+//   String tempCategory = '';
+//   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+//   FocusNode addCatFocusNode;
+//   final NotesService notesService = NotesService();
+//
+//   @override
+//   void initState() {
+//     updateList();
+//     addCatFocusNode = FocusNode();
+//     super.initState();
+//   }
+//
+//   void dispose() {
+//     addCatFocusNode.dispose();
+//     super.dispose();
+//   }
+//
+//   void updateList() async {
+//     list = await notesService.readNotes();
+//     setState(() {
+//       list.sort();
+//     });
+//   }
+//
+//   void undoDelete(String category, int index) async {
+//     await notesService.addNote(category, index: index);
+//     updateList();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         key: scaffoldKey,
+//         appBar: AppBar(
+//           title: MainLocalText(text: "Заметки"),
+//         ),
+//         body: Column(
+//           children: [
+//             SizedBox(height: 10),
+//             Expanded(
+//               child: list.isEmpty
+//                 ? Center(child: MainLocalText(text: 'заметок нет'))
+//                 : ListView.builder(
+//                     itemCount: list.length,
+//                     itemBuilder: (context, index) {
+//                       String category = list[index];
+//                       return Dismissible(
+//                         key: ValueKey(category),
+//                         child: Padding(
+//                         padding: EdgeInsets.only(left: 5, right: 5),
+//                           child: Column(
+//                             children: [
+//                               SizedBox(height: 25),
+//                               Container(
+//                                 child: Row(
+//                                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                                   children: [Expanded(child: SecondaryText(text: category))]
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                         direction: DismissDirection.endToStart,
+//                         onDismissed: (direction) async {
+//                           CustomSnackBar.show(
+//                               key: scaffoldKey,
+//                               context: context,
+//                               text: AppLocalizations.of(context).translate('Удалена заметка: ') + category,
+//                               textColor: Colors.white,
+//                               callBack: () {
+//                                 undoDelete(category, index);
+//                               });
+//
+//                           await notesService.deleteNote(category);
+//                           updateList();
+//                         },
+//                         background: Container(),
+//                         secondaryBackground: Container(
+//                           alignment: Alignment.centerRight,
+//                           color: Colors.redAccent,
+//                           child: Padding(
+//                             padding: EdgeInsets.only(right: 15),
+//                             child: Icon(Icons.clear, color: Colors.black54),
+//                           ),
+//                         ),
+//                       );
+//                     },
+//                   ),
+//             ),
+//             Padding(
+//               padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Expanded(
+//                     child: Container(
+//                       decoration: AppDecoration.boxDecoration(context),
+//                       child: TextFormField(
+//                         style: TextStyle(color: Theme.of(context).textTheme.displayLarge.color,),
+//                         controller: TextEditingController(),
+//                         decoration: InputDecoration(
+//                           hintText: AppLocalizations.of(context).translate('Добавьте заметку'),
+//                           hintStyle: TextStyle(color: AppColors.hint),
+//                           contentPadding: EdgeInsets.all(20.0),
+//                           border: addCatFocusNode.hasFocus
+//                             ? OutlineInputBorder(
+//                               borderRadius: BorderRadius.all(Radius.circular(15)),
+//                               borderSide: BorderSide(color: Colors.blue)
+//                               )
+//                             : InputBorder.none,
+//                         ),
+//                         onChanged: (v) => tempCategory = v,
+//                       ),
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.only(left: 25),
+//                     child: Container(
+//                       height: 60,
+//                       width: 60,
+//                       decoration: AppDecoration.boxDecoration(context),
+//                       child: IconButton(
+//                         icon: Icon(Icons.add,
+//                             //color: AppColors.textColor()
+//                         ),
+//                         onPressed: () async {
+//                           if (tempCategory == '') return;
+//                           //list.add(tempCategory);
+//                           tempCategory = '';
+//                           TextEditingController().clear();
+//                           await notesService.addNote(tempCategory);
+//                           //await Storage.saveNotesList(list);
+//                           updateList();
+//                         },
+//                       ),
+//                     ),
+//                   )
+//                 ]
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
